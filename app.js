4ba51aed3d068083eb26c9c37e2e1a3d99b7ddc5
Martin <martin.guzlej@gmail.com>
2020-06-21 20:09:42 +0100

    Comented out Backend JS it's working again 100% LOCALLY need to implement backend so it can be pushed on heroku

--- app.js
+++ app.js
@@ -1,96 +1,55 @@
-const express = require('express'),
-app = express();
+//const $ = require("./lib/jquery");
+// const express = require('express'),
+// app = express();
 
-app.set("view-engine", "ejs");
+// app.set("view-engine", "ejs");
 
-app.get("/", () => {
-  res.render("index");
-});
+// app.get("/", () => {
+//   res.render("index");
+// });
 
-app.listen(3000, () => {
-  console.log("Server on!")
-});
+// app.listen(3000, () => {
+//   console.log("Server on!")
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// jQuery settings
 $(document).ready(function(){
-    $('.multiple-items').slick({
-        prevArrow: '.arrow_prev',
-        nextArrow: '.arrow_next',
-        slidesToShow: 3,
-        autoplay: true,
-        arrows: true,
-        autoplaySpeed: 5000,
-        centerMode: true,
-        speed: 500,
-        swipeToSlide: true,
-        cssEase: 'linear',
-        responsive: [
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                centerMode: true,
-                centerPadding: '40px',
-                prevArrow: 'false',
-                nextArrow: 'false',
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                centerMode: true,
-                prevArrow: 'false',
-                nextArrow: 'false',
-                centerPadding: '40px',
-                slidesToScroll: 1
-              }
+  $('.multiple-items').slick({
+      prevArrow: '.arrow_prev',
+      nextArrow: '.arrow_next',
+      slidesToShow: 3,
+      autoplay: true,
+      arrows: true,
+      autoplaySpeed: 5000,
+      centerMode: true,
+      speed: 500,
+      swipeToSlide: true,
+      cssEase: 'linear',
+      responsive: [
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              centerMode: true,
+              centerPadding: '40px',
+              prevArrow: 'false',
+              nextArrow: 'false',
+              slidesToScroll: 2
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true,
+              prevArrow: 'false',
+              nextArrow: 'false',
+              centerPadding: '40px',
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+  });
 });
-
-// const slider = $(".multiple-items");
-
-// //Implementing navigation of slides using mouse scroll
-// slider.on('wheel', (function(e) {
-//   e.preventDefault();
-
-//   if (e.originalEvent.deltaY < 0) {
-//     $(this).slick('slickNext');
-//   } else {
-//     $(this).slick('slickPrev');
-//   }
-// }));
