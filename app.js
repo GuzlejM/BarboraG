1e2e3f7c9a068742c4349d88bc5d31b937e0b3d1
Martin <martin.guzlej@gmail.com>
2020-07-22 17:23:22 +0100

    Routes to Gallery done, styling bathroom

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+
+// Routes
+app.get("/bathroom", (req, res) => {
+ res.render("galleries/bathroom");
+});
+app.get("/chilloutzone", (req, res) => {
+ res.render("galleries/chillout");
+});
+app.get("/paulenhouse", (req, res) => {
+ res.render("galleries/paulhouse");
+});
+app.get("/childroom", (req, res) => {
+ res.render("galleries/chillout");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
